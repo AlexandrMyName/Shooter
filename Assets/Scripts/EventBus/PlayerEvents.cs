b1f64e53e7787092337a385c68ed91bc09cc2104
Mikhail Nemtinov <86906070+NeMikhail@users.noreply.github.com>
2023-09-21 23:28:59 +0300

    MiniFix

--- Assets/Scripts/EventBus/PlayerEvents.cs
+++ Assets/Scripts/EventBus/PlayerEvents.cs
@@ -5,11 +5,17 @@ namespace EventBus
     public static class PlayerEvents
     {
         public static event Action<int> OnGameEnded;
+        public static event Action OnGamePaused;
         public static event Action OnDead;
         
         public static void GameEnded(int score)
         {
             OnGameEnded?.Invoke(score);
+        }
+        
+        public static void PauseGame()
+        {
+            OnGamePaused?.Invoke();
         }
         
         public static void PlayerDead()
